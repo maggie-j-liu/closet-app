25dbcf728dc0de2a7fc4e35f76f1eda83fc4a139
maggie-j-liu <maggie.j.liu@gmail.com>
2021-06-25 19:21:52 -0700

    fix tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import firebase from '../firebase/initFirebase'
-// import '../styles/globals.css'
- import 'tailwindcss/tailwind.css'
+
 firebase()
 
 export default function Home() {
@@ -10,10 +9,9 @@ export default function Home() {
     
     <div className="container mx-auto">
         {/* tailwind is not loading */}
-        <h1 className="text-5xl lg:text-6xl font-bold text-teal-600 mb-2 lg:mb-6">Win your market</h1>
+        <h1 className="text-5xl lg:text-6xl font-bold text-blue-600 mb-2 lg:mb-6">Win your market</h1>
         <p class="text-gray-500">You have a new message!</p>
           The quick brown fox jumps over the lazy dog.
-        </p>
     </div>
   )
 }
