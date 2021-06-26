fbabccc145a00d6815bd4ffe99af744539838697
Dong Liu <dongliu0426@gmail.com>
2021-06-27 07:22:37 +0800

    Update index page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import FeaturedSection from '../components/IndexPage/FeaturedSection'
+import HeroSection from '../components/IndexPage/HeroSection'
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4">
-        <h1 className="text-5xl lg:text-6xl font-bold text-blue-600 mb-2 lg:mb-6">
-          Closet App
-        </h1>
-        <p className="text-gray-500">Upload, save, and search images.</p>
-      </div>
+    <>
+        <HeroSection />
+        <FeaturedSection />
+    </>
   );
 }
