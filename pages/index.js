f807287532c593209f7d7fa7adf7b856d3add5ba
Dong Liu <dongliu0426@gmail.com>
2021-06-26 04:36:44 -0700

    remove extra log in button

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="container mx-auto px-4">
-      <p><a href="/auth">LOG IN</a></p>
         <h1 className="text-5xl lg:text-6xl font-bold text-blue-600 mb-2 lg:mb-6">
           Closet App
         </h1>
