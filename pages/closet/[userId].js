7bc84efc92db96255470b3f6d3d6cefa449137f1
epicprogramming101 <46630586+epicprogramming101@users.noreply.github.com>
2021-06-26 16:20:14 -0700

    fix bubble sort

--- pages/closet/[userId].js
+++ pages/closet/[userId].js
@@ -34,31 +34,27 @@ const Closet = ({ userId, userCloset }) => {
   const [tags, setTags] = React.useState([]);
   // setTags([]);
   console.log(userCloset);
-  for (var j = 0; j < userCloset.length - 1; j++)
-    for (var i = 0; i < userCloset.length - 1; i++) {
+  for (var j = 0; j < userCloset.length; j++)
+    for (var i = 0; i < userCloset.length-1; i++) {
       if (siml(userCloset[i].tags, tags) > siml(userCloset[i + 1].tags, tags)) {
         const x = userCloset[i];
         userCloset[i] = userCloset[i + 1];
         userCloset[i + 1] = x;
       }
     }
-<<<<<<< Updated upstream
   firebase
     .firestore()
     .collection("users")
     .doc(user.id)
     .update({ id: user.id, name: user.name, email: user.email });
-=======
-  }
   const displayCloset = [];
-  for (var i = 0; i < userCloset.length-1; i ++) {
+  for (var i = 0; i < userCloset.length; i ++) {
     if (siml(userCloset[i].tags, tags) >= tags.length/2) {
       displayCloset.push(userCloset[i]);
     }
   }
 
   firebase.firestore().collection("users").doc(user.id).update({id: user.id, name: user.name, email: user.email})
->>>>>>> Stashed changes
   // no support for sharing closets yet
   if (userId != user.id) {
     router.replace({
