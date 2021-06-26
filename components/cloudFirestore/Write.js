6a863fb0cd3faf0355138d49422f0ec21f09e7ba
epicprogramming101 <46630586+epicprogramming101@users.noreply.github.com>
2021-06-25 21:56:07 -0700

    auth

---
+++ components/cloudFirestore/Write.js
@@ -0,0 +1,37 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+// import { useUser } from '../../firebase/useUser'
+
+const WriteToCloudFirestore = () => {
+    // const { user } = useUser()
+    const sendData = () => {
+        try {
+            firebase
+                .firestore()
+                .collection('myCollection')
+                .doc('my_document') // leave as .doc() for a random unique doc name to be assigned
+                .set({
+                    string_data: 'Christopher Che',
+                    number_data: 2,
+                    boolean_data: true,
+                    map_data: { stringInMap: 'Hi', numberInMap: 7 },
+                    array_data: ['text', 4],
+                    null_data: null,
+                    time_stamp: firebase.firestore.Timestamp.fromDate(new Date('December 17, 1995 03:24:00')),
+                    geo_point: new firebase.firestore.GeoPoint(34.714322, -131.468435)
+                })
+                .then(alert('Data was successfully sent to cloud firestore!'))
+        } catch (error) {
+            console.log(error)
+            alert(error)
+        }
+    }
+
+    return (
+        <div style={{ margin: '5px 0' }}>
+            <button onClick={sendData} style={{ width: '100%' }}>Send Data To Cloud Firestore</button>
+        </div>
+    )
+}
+
+export default WriteToCloudFirestore
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import firebase from '../firebase/initFirebase'
-
-firebase()
+import WriteToCloudFirestore from '../components/cloudFirestore/Write'
+import ReadDataFromCloudFirestore from '../components/cloudFirestore/Read'
 
 export default function Home() {
   return (
-    
     <div className="container mx-auto px-4">
+      <p><a href="/auth">LOG IN</a></p>
+      <WriteToCloudFirestore />
+      <ReadDataFromCloudFirestore />
         <h1 className="text-5xl lg:text-6xl font-bold text-blue-600 mb-2 lg:mb-6">
           Closet App
         </h1>
-        <p class="text-gray-500">
+        <p className = "text-gray-500">
           Upload, save, and search images.
         </p>
     </div>
