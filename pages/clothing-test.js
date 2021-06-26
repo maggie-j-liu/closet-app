f968771a47d3b2f4ac95daebfbc2ed9517edda3a
Dong Liu <dongliu0426@gmail.com>
2021-06-26 16:05:09 +0800

    Fix stuff

--- pages/clothing-test.js
+++ pages/clothing-test.js
@@ -1,14 +1,15 @@
 import React, { Fragment } from "react";
 
 export default function ClothingTest() {
-	const [image, setImage] = React.useState();
 	const [file, setFile] = React.useState();
 	const read = (event) => {
 	  const reader = new FileReader()
 	  reader.readAsDataURL(event.target.files[0]);
 	   reader.addEventListener("load", function () {
 	   	let url = reader.result;
+	   	/* extract the prefict to get base64 */
 	   	url = url.replace('data:image/jpeg;base64,', '');
+	   	/* fetch the data */
 	   	fetch(`https://clothing-api-server.vercel.app/fetch`, {
 	   	  method: 'POST',
 	      headers: {
@@ -18,8 +19,10 @@ export default function ClothingTest() {
 	      	base64: url
 	      })
 		   }).then(data => {
-		   	console.log(data.body);
-		   	setFile(data.body);
+		   	data.json().then(stuff => {
+		   		console.log(JSON.stringify(stuff));
+		   		setFile(JSON.stringify(stuff))
+		   	})
 		   })
 		 }, false);
 	}
@@ -31,13 +34,10 @@ export default function ClothingTest() {
         className={"p-0.5 rounded-md focus-ring w-min"}
         onChange={(e) => {
         	console.log(URL.createObjectURL(e.target.files[0]));
-			setImage(URL.createObjectURL(e.target.files[0]));
-        	/* console.log(e.target.files[0]); */
 			read(e);
         }}
       />
-      <img src={image} className={"w-3/4 m-auto"} />
-      <pre src={file}></pre>
+      <pre>{file}</pre>
       </div>
      )
 }
