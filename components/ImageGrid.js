6a8caadb41af27f5f3734329d6f6595776cc5c6d
maggie-j-liu <maggie.j.liu@gmail.com>
2021-06-26 14:06:20 -0700

    show tags

--- components/ImageGrid.js
+++ components/ImageGrid.js
@@ -1,10 +1,42 @@
+const Image = ({ url, tags }) => {
+  console.log("image tags", tags);
+  return (
+    <div className={"relative w-full h-full group flex justify-center"}>
+      <img src={url} className={"self-center"} />
+      <div
+        className={
+          "absolute inset-0 bg-gray-700 opacity-0 group-hover:opacity-60"
+        }
+      />
+      <div
+        className={"absolute inset-0 flex flex-col items-center justify-center"}
+      >
+        <div
+          className={"flex flex-wrap items-center justify-center gap-2 px-6"}
+        >
+          {tags.map((tag, idx) => (
+            <div
+              key={idx}
+              className={
+                "opacity-0 group-hover:opacity-100 h-max bg-indigo-300/70 px-1 py-0.5 rounded-md"
+              }
+            >
+              {tag}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const ImageGrid = ({ images }) => {
   console.log(images);
   images = images.reverse();
   return (
     <div className={"grid grid-cols-4 gap-4 px-8"}>
       {images.map(({ url, tags }, idx) => (
-        <img key={idx} src={url} />
+        <Image key={idx} url={url} tags={tags} />
       ))}
     </div>
   );
